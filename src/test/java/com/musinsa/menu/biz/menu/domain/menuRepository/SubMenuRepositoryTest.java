2e58312195629cc3d59133c326f6ff2716716ee7
dev-seokho <loyu78@gmail.com>
2023-07-09 00:04:36 +0900

    Test: 서브 메뉴에 이미 타이틀이 존재할 때 True 반환 레포지토리 테스트 추가

---
+++ src/test/java/com/musinsa/menu/biz/menu/domain/menuRepository/SubMenuRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.musinsa.menu.biz.menu.domain.menuRepository;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.jdbc.EmbeddedDatabaseConnection.H2;
+
+import com.musinsa.menu.biz.menu.domain.entity.Menu;
+import com.musinsa.menu.biz.menu.domain.entity.SubMenu;
+import com.musinsa.menu.biz.menu.domain.repository.MenuRepository;
+import com.musinsa.menu.biz.menu.domain.repository.SubMenuRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@AutoConfigureTestDatabase(connection = H2)
+@DataJpaTest
+@ActiveProfiles("test")
+public class SubMenuRepositoryTest {
+
+    @Autowired
+    SubMenuRepository subMenuRepository;
+    @Autowired
+    MenuRepository menuRepository;
+    long menuId;
+
+    @BeforeEach
+    void beforeEach() {
+        String title = "상의";
+        Menu menu = Menu.builder()
+            .title(title)
+            .link("/top")
+            .build();
+
+        this.menuId = menuRepository.save(menu).getId();
+    }
+
+    @Test
+    @DisplayName("서브 메뉴에 이미 타이틀이 존재할 때 true 반환")
+    void existsByTitleTest() {
+        //given
+        String title = "상의";
+
+        SubMenu subMenu = SubMenu.builder()
+            .menuId(this.menuId)
+            .title(title)
+            .build();
+
+        subMenuRepository.save(subMenu);
+
+        //when
+        boolean existsTitle = subMenuRepository.existsByTitle(title);
+
+        //then
+        assertTrue(existsTitle);
+    }
+}
