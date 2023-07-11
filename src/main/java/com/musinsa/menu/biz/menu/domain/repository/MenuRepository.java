d923f77dfa169d6db136b9565360a0bf8b1ca2bc
dev-seokho <loyu78@gmail.com>
2023-07-12 01:28:48 +0900

    Refactor: @Param 어노테이션 추가

--- src/main/java/com/musinsa/menu/biz/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/musinsa/menu/biz/menu/domain/repository/MenuRepository.java
@@ -4,10 +4,11 @@ import com.musinsa.menu.biz.menu.domain.entity.Menu;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
-    boolean existsByTitle(String title);
-    boolean existsByLink(String link);
+    boolean existsByTitle(@Param("title") String title);
+    boolean existsByLink(@Param("link") String link);
     Slice<Menu> findSliceBy(PageRequest pageRequest);
 }
--- src/main/java/com/musinsa/menu/biz/menu/domain/repository/SubMenuRepository.java
+++ src/main/java/com/musinsa/menu/biz/menu/domain/repository/SubMenuRepository.java
@@ -2,8 +2,9 @@ package com.musinsa.menu.biz.menu.domain.repository;
 
 import com.musinsa.menu.biz.menu.domain.entity.SubMenu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface SubMenuRepository extends JpaRepository<SubMenu, Long> {
 
-    boolean existsByTitle(String title);
+    boolean existsByTitle(@Param("title") String title);
 }
